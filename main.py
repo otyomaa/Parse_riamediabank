666107a1748b0aac3cb136269c5f496cc2312534
Artjom <otyomaa@gmail.com>
2023-08-18 14:41:50 +0300

    Update commit

--- main.py
+++ main.py
@@ -12,38 +12,29 @@ def parse():
     # URL сайта
     base_url = "https://riamediabank.ru"
     story_url = "/search/?selection=story"
-    # Путь к папке, куда будут сохранены изображения
+
     save_folder = "downloaded_images"
 
-    # Создание папки для сохранения изображений
     if not os.path.exists(save_folder):
         os.makedirs(save_folder)
 
-    # Количество изображений для скачивания
     num_images_to_download = int(input("Введите количество изображений для скачивания: "))
 
-    # Получение содержимого страницы
     soup = BeautifulSoup(requests.get(urljoin(base_url, story_url), headers=headers).text, "html.parser")
 
-    # Получение списка ссылок статей
     a_tags = soup.find_all("a", class_="list-item__link")
 
-    # Счетчик скачанных изображений
     downloaded_count = 0
 
-    # Перебор тегов <a>
     for a_tag in a_tags:
         if downloaded_count >= num_images_to_download:
             break
 
         a_page_soup = BeautifulSoup(requests.get(urljoin(base_url, a_tag.get("href")), headers=headers).text,
                                     "html.parser")
-        # a_page = urljoin(base_url, a_tag.get("href"))
-        # a_page_soup = BeautifulSoup(requests.get(a_page, headers=headers).text, "html.parser")
 
         a_tags_page = a_page_soup.find_all("a", class_="list-item__link")
 
-        # Перебор тегов <a> со ссылками на изображение в статье
         for a_tag_page in a_tags_page:
             if downloaded_count >= num_images_to_download:
                 break
@@ -55,9 +46,8 @@ def parse():
 
             h3_tag = pic_soup.find("div", class_="media__col_l").find("h3", class_="media__description")
 
-            img_name = f"{'_'.join(h3_tag.string.split()[:30])}.jpg"  # Имя файла с расширением
+            img_name = f"{'_'.join(h3_tag.string.split()[:30])}.jpg"
 
-            # Загрузка изображения
             img_response = requests.get(img_src, headers=headers)
             if img_response.status_code == 200:
                 img_path = os.path.join(save_folder, img_name)
